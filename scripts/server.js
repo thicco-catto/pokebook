71993e49744a3995c028b339eabfd3df5a06c93d
cristobalmartinarenas <crimarare@uma.es>
2022-05-11 18:03:17 +0200

    Added a couple methods to server.js

--- scripts/server.js
+++ scripts/server.js
@@ -14,9 +14,52 @@ firebase.initializeApp(firebaseConfig);
 // Initialize Cloud Firestore and get a reference to the service
 const db = firebase.firestore();
 
-//Get data
-db.collection("user").get().then((querySnapshot) => {
-    querySnapshot.forEach((doc) => {
-        document.getElementById("texto").textContent = doc.data().nombre;
-    });
-});
+function GetUserById(id){
+    return new Promise(resolve =>
+        db.collection("user").get().then((querySnapshot) => {
+            querySnapshot.forEach((doc) => {
+                if(doc.data().userID === id){
+                    console.log("hola");
+                    resolve(doc.data());
+                }
+            })
+
+            resolve(null);
+        })
+    );
+}
+
+
+function GetMaxUserId(){
+    let maxId = 0;
+    return new Promise(resolve =>
+        db.collection("user").get().then((querySnapshot) => {
+            querySnapshot.forEach((doc) => {
+                if(doc.data().userID > maxId){
+                    maxId = doc.data().userID;
+                }
+            })
+
+            resolve(maxId);
+        })
+    );
+}
+
+function AddNewUser(nick, password, email, dob){
+    return new Promise(resolve =>
+        db.collection("user").get().then((querySnapshot) => {querySnapshot.forEach((doc) => {if(doc.data().userID > maxId){maxId = doc.data().userID;}});resolve(maxId);})
+    );
+}
+
+function CheckUser(nick, pass){
+    return new Promise(resolve =>
+        db.collection("user").get().then((querySnapshot) => {
+            querySnapshot.forEach((doc) => {
+                if(doc.data().nick === nick && doc.data().password === pass){
+                    resolve(true)
+                }
+            })
+            resolve(false)
+        })
+    );
+}
