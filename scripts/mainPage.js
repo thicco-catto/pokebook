d7e5aadf2d49eaf129bb7eaae2bf9add2ff7b0e4
cristobalmartinarenas <crimarare@uma.es>
2022-05-25 20:17:17 +0200

    Fixed mini button on main page not working properly

--- scripts/mainPage.js
+++ scripts/mainPage.js
@@ -1,6 +1,12 @@
 async function onLoad(event){
     const posts = await GetPosts();
 
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+
+    const user = urlParams.get("user");
+    document.getElementById("new-post-mini").href = `crear_post.html?user=${user}`
+
     let postArray = [];
     posts.forEach(post => {
         postArray.push(post);
