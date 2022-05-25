fce37dcbad86730456db49970de9965c6c708e93
cristobalmartinarenas <crimarare@uma.es>
2022-05-25 10:25:42 +0200

    Added more info to profile

--- scripts/profile.js
+++ scripts/profile.js
@@ -2,9 +2,27 @@ async function onLoad(event){
     const queryString = window.location.search;
     const urlParams = new URLSearchParams(queryString);
 
-    const user = urlParams.get("userProfile");
+    const selfUserNick = urlParams.get("user");
+    const userNick = urlParams.get("userProfile");
 
-    const posts = await GetPostsByUser(user);
+    const user = await GetUserByNick(userNick);
+
+    const nickText = document.getElementById("user-nick");
+    nickText.textContent = user.id;
+
+    const emailText = document.getElementById("user-email");
+    emailText.textContent = user.data().email;
+
+    const followButton = document.getElementById("follow-button");
+    if(selfUserNick === userNick){
+        followButton.textContent = "Editar";
+        followButton.onclick = () => console.log(selfUserNick + " SAME");
+    }else{
+        followButton.textContent = "Seguir";
+        followButton.onclick = () => console.log(userNick + " follow");
+    }
+
+    const posts = await GetPostsByUser(userNick);
 
     let postArray = [];
     console.log(posts.size);
