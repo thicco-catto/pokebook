aa0cf06fb4a4104f84613546770f1c91b50f43a5
cristobalmartinarenas <crimarare@uma.es>
2022-05-27 18:11:20 +0200

    Added reposts to main page

--- scripts/mainPage.js
+++ scripts/mainPage.js
@@ -1,19 +1,47 @@
 async function onLoad(event){
-    const posts = await GetPosts();
-
     const queryString = window.location.search;
     const urlParams = new URLSearchParams(queryString);
 
-    const user = urlParams.get("user");
-    document.getElementById("new-post-mini").href = `crear_post.html?user=${user}`
+    const selfUser = urlParams.get("user");
+    document.getElementById("new-post-mini").href = `crear_post.html?user=${selfUser}`
 
-    let postArray = [];
-    posts.forEach(post => {
-        postArray.push(post);
-    });
+    const followedUsers = await GetUserFollowed(selfUser);
+    const postArray = [];
+    const repostIds = [];
+
+    for (let i = 0; i < followedUsers.size; i++) {
+        const user = followedUsers.docs[i];
+        const posts = await GetPostsByUser(user.id);
+
+        posts.forEach(post => {
+            postArray.push(post);
+        });
+    }
+
+    for (let i = 0; i < followedUsers.size; i++) {
+        const user = followedUsers.docs[i];
+        const repostPosts = await GetRepostsPerUser(user.id);
+
+        for (let o = 0; o < repostPosts.size; o++) {
+            const repostPostId = repostPosts.docs[i];
+
+            let isIncluded = false;
+            postArray.forEach(x => {
+                if(x.id === repostPostId.id){
+                    isIncluded = true;
+                }
+            });
+    
+            if(isIncluded){ continue; }
+    
+            const post = await GetPostById(repostPostId.id);
+            repostIds.push(new repostAndUser(repostPostId.id, user.id));
+            postArray.push(post);
+        }
+    }
 
     postArray.sort((a, b) => b.data().postDate - a.data().postDate);
 
-    await renderPosts(postArray);
+    renderPosts(postArray, repostIds);
 }
 addEventListener("DOMContentLoaded", onLoad);
